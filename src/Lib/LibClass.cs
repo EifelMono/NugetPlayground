0f359232f303f82b06d977d2877316b89ac8c9ca
Andreas Klapperich <andreas@klapperich.de>
2022-12-22 12:36:39 +0100

    CHG: Windows/Mac/Linux ...

--- src/Lib/LibClass.cs
+++ src/Lib/LibClass.cs
@@ -10,19 +10,13 @@ public class LibClass
     public static void Infos()
     {
         Console.WriteLine($"Lib Infos");
-        ;
+              Console.WriteLine($"AppContext.BaseDirectory {AppContext.BaseDirectory}");
         Console.WriteLine($"Enty Assembly {Assembly.GetEntryAssembly()!.FullName}");
-        ;
         Console.WriteLine($"Calling Assembly {Assembly.GetCallingAssembly()!.FullName}");
-        ;
         Console.WriteLine($"Executing Assembly {Assembly.GetExecutingAssembly()!.FullName}");
-        ;
         Console.WriteLine($"AppText exist {System.IO.File.Exists(s_appText)}");
-        ;
         Console.WriteLine($"LibText exist {System.IO.File.Exists(s_libText)}");
-        ;
         Console.WriteLine($"OtherLibTxt_OtherLibText exist {System.IO.File.Exists(s_otherLibTxt_OtherLibText)}");
-        ;
         Console.WriteLine($"\r\n");
     }
 }
